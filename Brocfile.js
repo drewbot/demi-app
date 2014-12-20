abc1a0c162194681c9be9a865be122c4e79ca8f8
Drew Botka <hellodrewbot@gmail.com>
2014-12-20 12:15:09 -0500

    added glyphicon to the sm logout button and cleaned up the filter module padding and naming

--- Brocfile.js
+++ Brocfile.js
@@ -18,3 +18,6 @@ var app = new EmberApp();
 // along with the exports of each module as its value.
 
 module.exports = app.toTree();
+
+// bootstrap
+app.import('bower_components/bootstrap/dist/css/bootstrap.css');
