0a0eec213a3aa4c7b820f424f19d9c3027678710
Drew Botka <hellodrewbot@gmail.com>
2015-01-30 13:17:19 -0500

    update to baseurl in config environment.js

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'demi-app',
     environment: environment,
-    baseURL: '',
+    baseURL: 'demi-app/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
