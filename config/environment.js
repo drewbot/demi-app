a5d677a02cfa5a0c8ed7a84a5f70f6c50446249e
Drew Botka <hellodrewbot@gmail.com>
2015-01-31 09:42:57 -0500

    changed config environment.js baseurl back to original

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'demi-app',
     environment: environment,
-    baseURL: 'demi-app',
+    baseURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
